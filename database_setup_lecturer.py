dfee92b71fe288e86c880b3719fa603d53fe626b
pattaraporn punya <pattaraporngcgmail.com>
2017-10-22 15:25:45 +0700

    databaselecturer

--- database_setup_lecturer.py
+++ database_setup_lecturer.py
@@ -8,16 +8,21 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-
-    name = Column(String(80),nullable = False)
+    user = Column(String(80),nullable = False)
     id = Column(Integer, primary_key = True)
     #password = Column(String(80),nullable = False)
+
 class Password(Base):
     __tablename__ = 'password'
     password = Column(String(80),nullable = False)
     password_id = Column(Integer,ForeignKey('user.id'))
-    id = Column(Integer,primary_key = True)
-    user = relationship(User)
+    password_relationship = relationship(User)
 
-engine = create_engine('sqlite:///account.db')
+class Name_lecturer(Base):
+    __tablename__ = 'name_lecturer'
+    namelecturer = Column(String(80),nullable = False)
+    namelecturer_id = Column(Integer,ForeignKey('user.id'))
+    namelecturer_relationship = relationship(User)
+
+engine = create_engine('sqlite:///lecturer.db')
 Base.metadata.create_all(engine)

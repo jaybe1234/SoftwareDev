1d2be26334f10dbd0eb5fc4aaee42d062fefd656
Jaybe <ktbambam@icloud.com>
2017-10-23 04:48:10 +0700

    Revert "Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/justinunited/SoftwareDev"""""
    This reverts commit fdcb99711806949b2daace2cac9bcb8a4c8643fd.

---
+++ nutty_create_student.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine , Float
+from sqlalchemy.orm import sessionmaker
+from nutty_database_setup import Base , Student
+engine = create_engine('sqlite:///student.db')
+Base.metadata.create_all(engine)
+DBsession = sessionmaker(bind = engine)
+session = DBsession()
+
+def create_student(numid,user,password,name,year,sec,gpax):
+    new_user = Student(id_student = numid,name_student = name,user_student = user,password_student = password, year_student = int(year) , section_student = sec , gpax_student  = Float(gpax))
+    session.add(new_user)
+    session.commit()

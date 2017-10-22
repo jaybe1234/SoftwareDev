c39ee503fd6793ff25c62838388fd6bd395a92a1
Jaybe <ktbambam@icloud.com>
2017-10-23 04:48:55 +0700

    Revert "Revert "Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/justinunited/SoftwareDev""""""
    This reverts commit 5f9de5db1ae583576ec262f6c959db9170cb6897.

--- nutty_create_task.py
+++ nutty_create_task.py
@@ -8,5 +8,10 @@ session = DBsession()
 
 def create_task(weight,des,fscore):
     new_task = Task(weight = weight,description = des,fullscore_task = int(fscore))
+<<<<<<< HEAD
     session.add(new_task)
     session.commit()
+=======
+    sesstion.add(new_task)
+    sesstion.commit()
+>>>>>>> parent of 5f9de5d... Revert "Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/justinunited/SoftwareDev"""""

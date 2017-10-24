f29beb585668bf4f07727ccc0a2b187523900d9e
Odiosaser <s56010024@hotmail.com>
2017-10-25 00:48:37 +0700

    Esthub
    Add data Grouping , Group , Task , Score

--- AddData.py
+++ AddData.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from DatabaseSetup import Base,Lecturer,Student,Enrollment,Subject
+from DatabaseSetup import Base,Lecturer,Student,Enrollment,Subject,Grouping,Group,Task,Score
 #from and NameOfPythonFile
 engine = create_engine('sqlite:///database.db')
 Base.metadata.bind = engine
@@ -8,7 +8,7 @@ DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
 def create_student(id,user,password,name,year,section,gpax):
-    new_student = Student(id = id, user_student = user, password_student = password, name_student =  name,year_student = year, section_student = section , gpax_student = gpax)
+    new_student = Student(id_student = id, user_student = user, password_student = password, name_student =  name,year_student = year, section_student = section , gpax_student = gpax)
     session.add(new_student)
     session.commit()
     return
@@ -17,7 +17,7 @@ create_student(59340500005,'justinunited','12345','Kanut',2,'A',4)
 create_student(59340500047,'Herokids','12345','Phusana',2,'B',1)
 
 def create_lecturer(id,user,password,name):
-    new_lecturer = Lecturer(id = id, user_lecturer = user, password_lecturer = password, name_lecturer =  name)
+    new_lecturer = Lecturer(id_lecturer = id, user_lecturer = user, password_lecturer = password, name_lecturer =  name)
     session.add(new_lecturer)
     session.commit()
     return
@@ -36,25 +36,50 @@ create_subject('Computer Programming','FRA142')
 create_subject('Digital','FRA221')
 create_subject('Sensor&Actuator','FRA222')
 
-def create_enrollment(id,code, student_id ,lecturer_id ):
-    new_enrollment = Enrollment(id_enrollment = id , subject_enrollment = code , student_id_enrollment = student_id,lecturer_id_enrollment = lecturer_id)
+def create_enrollment(subject_code, student_id ,lecturer_id ):
+    subject_code = session.query(Subject).filter_by(code_subject= subject_code)[0]
+    new_enrollment = Enrollment(subject_enrollment = subject_code , student_id_enrollment = student_id,lecturer_id_enrollment = lecturer_id)
     session.add(new_enrollment)
     session.commit()
     return
-#create_enrollment()
-
-#cannot specific subject
-def read_subject():
-    subjects = session.query(Subject).all()
-    for subject in subjects:
-        result = subject
-    return result
-print (read_subject())
-create_enrollment(1,read_subject(), 59340500060 , None)
-create_enrollment(2,read_subject(), 59340500047 , None)
-create_enrollment(3,read_subject(), 59340500005 , None)
-create_enrollment(4,read_subject(), None , 1)
+create_enrollment("FRA241", 59340500060 , 1)
+create_enrollment("FRA241", 59340500047 , None)
+create_enrollment("FRA142", 59340500005 , None)
+create_enrollment("FRA222", None , 1)
 # def read_lecturer():
 #     lecturers = session.query(Lecturer).all()
 #     for lecturer in lecturers:#         print (lecturer.name_lecturer)
 #     return
+
+def create_grouping(grouping_id ,grouping_name,grouping_type,subject_code):
+    new_grouping = Grouping(id_grouping=grouping_id,name_grouping=grouping_name,type_grouping=grouping_type,subject_code_grouping=subject_code)
+    session.add(new_grouping)
+    session.commit()
+    return
+create_grouping(1,'Grouping by GPAX','GPAX','FRA241')
+
+def create_group(grouping_id,student_id,group_id):
+    grouping_id = session.query(Grouping).filter_by(id_grouping=grouping_id)[0]
+    new_group = Group(student_id_group = student_id ,group_id_group = group_id,grouping_group = grouping_id)
+    session.add(new_group)
+    session.commit()
+    return
+create_group(1,59340500060,'B01')
+
+def create_task(grouping_id,task_id,weight):
+    grouping_id = session.query(Grouping).filter_by(id_grouping=grouping_id)[0]
+    new_task = Task(id_task = task_id , weight_task = weight ,grouping_task = grouping_id)
+    session.add(new_task)
+    session.commit()
+    return
+create_task(1,'Lab1',5)
+
+def create_score(task_id,student_id,score):
+    task_id = session.query(Task).filter_by(id_task=task_id)[0]
+    new_score = Score(score_score = score,student_id_score = student_id,task_score = task_id)
+    session.add(new_score)
+    session.commit()
+    return
+create_score('Lab1',59340500060,99.9)
+
+

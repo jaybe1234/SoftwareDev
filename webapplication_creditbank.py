1c46d0a8fdeeee1e228beef493d03e04b6de1967
Jaybe <ktbambam@icloud.com>
2017-10-31 02:33:18 +0700

    fix bug

--- webapplication_creditbank.py
+++ webapplication_creditbank.py
@@ -1,15 +1,19 @@
 import os
+import smtplib
+import random
+from email.mime.text import MIMEText
 from flask import Flask , render_template , request , redirect , url_for ,flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from Database.DatabaseSetup import Base,Lecturer,Student,Enrollment,Subject,Grouping,Group,Task,Score,Credit
+
 app = Flask(__name__)
 engine = create_engine('sqlite:///database.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
-@app.route('/students/<string:subject_code>/<string:task_name>/',methods=['GET','POST'])
+@app.route("/<string:subject_code>/<string:task_name>",methods=['GET','POST'])
 def login(subject_code,task_name):
     if request.method == 'POST':
         student_id = request.form['student_id']
@@ -20,27 +24,44 @@ def login(subject_code,task_name):
                 grouping_object = i.grouping_task
                 group_id = session.query(Group).filter_by(grouping_group = grouping_object,student_id_group = student_id)[0].group_id_group
                 credit = session.query(Credit).filter_by(group_id_credit = group_id)[0].credit
-                return redirect(url_for('submitcode',student_id = student_id,subject_code = subject_code,task_name=task_name,credit_bank=credit,number=0))
+                code = ''
+                alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+                            'r', 's',
+                            't',
+                            'u', 'v', 'w', 'x', 'y', 'z']
+                for i in range(0, 6):
+                    number = random.randint(1, 25)
+                    code = code + alphabet[number]
+                # Create a text/plain message
+                msg = MIMEText(code)
+                msg['Subject'] = 'SUBMIT CODE FOR CREDITBANK!!'
+                msg['From'] = 's59340500060@hotmail.com'
+                msg['To'] = 'kakan002@hotmail.com'
+                s = smtplib.SMTP('smtp.live.com', 587)
+                s.ehlo()
+                s.starttls()
+                s.login('s59340500060@hotmail.com', '@f36oxeb4S')
+                s.sendmail('s59340500060@hotmail.com', email, "## CODE IS : " + msg.as_string())
+                s.close()
+                return redirect(url_for('submitcode',student_id = student_id,subject_code = subject_code,task_name=task_name,credit_bank=credit,number=0,email=email,code=code))
     else:
         return render_template('04_creditbank-login.html',subject_code=subject_code,task_name=task_name)
-@app.route('/students/<string:subject_code>/<string:task_name>/<int:student_id>/<int:credit_bank>/verify/<int:number>/',methods=['GET','POST'])
-def submitcode(subject_code,task_name,student_id,credit_bank,number):
-    code = 'verify'
+
+@app.route("/<string:subject_code>/<string:task_name>/<int:student_id>/<int:credit_bank>/<int:number>/<string:email>/<string:code>/<verify>",methods=['GET','POST'])
+def submitcode(subject_code,task_name,student_id,credit_bank,number,email,code):
     if request.method == 'POST':
         submit_code = request.form['E-mail address']
         if submit_code == code:
             return redirect(url_for('member', student_id=student_id, subject_code=subject_code, task_name=task_name,credit_bank=credit_bank))
         else:
-            number = number + 1
             if number>=4:
-                return render_template('04_creditbank-login.html', subject_code=subject_code, task_name=task_name)
-            return render_template('04_creditbank-login-submitcode.html', subject_code=subject_code,task_name=task_name, student_id=student_id, credit_bank=credit_bank,number=number)
-    else:
-        return render_template('04_creditbank-login-submitcode.html', subject_code=subject_code, task_name=task_name,student_id=student_id,credit_bank=credit_bank,number=1)
+                return redirect(url_for('login' , subject_code=subject_code , task_name=task_name))
+    number = number + 1
+    return render_template('04_creditbank-login-submitcode.html', subject_code=subject_code, task_name=task_name,student_id=student_id,credit_bank=credit_bank,number=number,email = email,code=code)
 
 
 
-@app.route('/students/<string:subject_code>/<string:task_name>/<int:student_id>/<int:credit_bank>/',methods=['GET','POST'])
+@app.route('/<string:subject_code>/<string:task_name>/<int:student_id>/<int:credit_bank>/member',methods=['GET','POST'])
 def member(subject_code,task_name,student_id,credit_bank):
     # return subject_code
     grouping_id_task = session.query(Task).filter_by(name_task=task_name)
@@ -62,11 +83,11 @@ def member(subject_code,task_name,student_id,credit_bank):
     else:
         return render_template('04_creditbank.html', groups=groups, credit_bank=credit_bank, student_id=student_id, length=length,task_name=task_name,subject_code=subject_code)
 
-@app.route('/students/thankyou/')
+@app.route('/thankyou')
 def thankyou():
-    return "THANK YOU :)"
+    return "Enjoy your score :P"
+
 
 if __name__ == "__main__":
-    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0',port = 5000)

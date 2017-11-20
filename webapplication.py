4d96532e4ae287fa775539e303cd901a28ebed49
pattaraporn punya <pattaraporngcgmail.com>
2017-11-21 03:05:05 +0700

    type_sort

--- webapplication.py
+++ webapplication.py
@@ -137,17 +137,17 @@ def addTask(username, subject_code):
             create_score(task.id_task, i.id_student, 0)
     return redirect(url_for('subject', username = username, subject_code = subject_code))
 
-@app.route('/<string:username>/<string:subject_code>/<string:lec_id>/remove_grouping', methods = ['GET', 'POST'])
-def removeLec(username,subject_code,lec_id):
+@app.route('/<string:username>/<string:subject_code>/<string:lec_id>/<string:type_sort>/remove_grouping', methods = ['GET', 'POST'])
+def removeLec(username,subject_code,lec_id,type_sort):
     if request.method == 'POST':
         delete_lecturer_enrollment(lec_id, subject_code)
-        return redirect(url_for('subject', username = username, subject_code = subject_code))
+        return redirect(url_for('subject', username = username, subject_code = subject_code,type_sort = type_sort))
 
-@app.route('/<string:username>/<string:subject_code>/<int:grouping_id>/remove_grouping', methods = ['GET', 'POST'])
-def removeGrouping(username,subject_code,grouping_id):
+@app.route('/<string:username>/<string:subject_code>/<int:grouping_id>/<string:type_sort>/remove_grouping', methods = ['GET', 'POST'])
+def removeGrouping(username,subject_code,grouping_id,type_sort):
     if request.method == 'POST':
         delete_grouping(grouping_id)
-        return redirect(url_for('subject', username = username, subject_code = subject_code))
+        return redirect(url_for('subject', username = username, subject_code = subject_code,type_sort = type_sort ))
 
 @app.route('/<string:username>/<string:subject_code>/<int:task_id>/remove_task', methods = ['GET', 'POST'])
 def removeTask(username,subject_code,task_id):

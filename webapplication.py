c717180e67d7cc44b3cef47e68a609a1fee68a2b
Jaybe <ktbambam@icloud.com>
2017-11-22 03:33:05 +0700

    manage student

--- webapplication.py
+++ webapplication.py
@@ -182,7 +182,10 @@ def removeTask(username,subject_code,task_id,type_sort):
 
 @app.route('/<string:username>/<string:subject_code>/Manage_student', methods = ['GET', 'POST'])
 def manageStudentList(username, subject_code):
-    return render_template('03_manage_student.html', username = username, subject_code = subject_code)
+    lecturerList = getLecturerList(subject_code)
+    groupingList = getGrouping(subject_code)
+    taskList = getTask(subject_code)
+    return render_template('03_manage_student.html', username = username, subject_code = subject_code, lecturerList = lecturerList, groupingList = groupingList, taskList = taskList)
 
 @app.route('/<string:username>/<string:subject_code>/<int:student_id>/<string:task_name>/<string:type_sort>/edit' , methods = ['GET' , 'POST'])
 def editScore(username,subject_code,student_id,task_name,type_sort=None):

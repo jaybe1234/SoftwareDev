59237a5ef1becd20ab4fbbcd291b6df21af6958e
KarnPH <31175834+KarnPH@users.noreply.github.com>
2017-11-30 04:23:57 +0700

    karn

--- database/getFunction.py
+++ database/getFunction.py
@@ -344,3 +344,30 @@ def grouping_studentid(group_from,group_num,subjectCode,grouping_name, group_pre
                 one = sorted_B[0]
                 create_group(grouping_id, one.id_student, group_prefix + '_B' + '#' + str(a + num_group_in_B - remain_B))
                 sorted_B.remove(one)
+
+def getGroupList(subject_code, grouping_name):
+    grouping = session.query(Grouping).filter_by(name_grouping = grouping_name, subject_code_grouping = subject_code).one()
+    grouplist = session.query(Group).filter_by(grouping_id_group = grouping.id_grouping)
+    name_group_list = []
+    for i in grouplist:
+        if i.group_id_group not in name_group_list:
+            name_group_list.append(i.group_id_group)
+    return name_group_list
+
+def studentInGroupList(subject_code,grouping_name,getgrouplist):
+    grouping = session.query(Grouping).filter_by(name_grouping = grouping_name, subject_code_grouping = subject_code).one()
+    member_group = []
+    for i in getgrouplist:
+        member = []
+        group = list(session.query(Group).filter_by(group_id_group = i, grouping_id_group = grouping.id_grouping))
+        for a in group:
+            student = session.query(Student).filter_by(id_student = a.student_id_group).one()
+            member.append(student)
+        member_group.append(member)
+    return member_group
+
+def lenInList(lis):
+    num_list = []
+    for i in lis:
+        num_list.append(len(i))
+    return num_list
